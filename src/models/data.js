73fb969671855ce5da790a82e85311e74ccdb2be
Nika K <firstnamenika@gmail.com>
2024-05-06 08:10:16 -0400

    pruning

---
+++ src/models/data.js
@@ -0,0 +1,34 @@
+const data = { 
+    1: {
+        name: "Learn JavaScript",
+        detals: "Build full stack apps",
+        isCompleted: false
+    },
+    2: {
+        name: "Learn TypeScript",
+        detals: "Build secure apps",
+        isCompleted: false
+    },
+    3: {
+        name: "Learn MongoDB",
+        detals: "Learn to manage data",
+        isCompleted: false
+    },
+    4: {
+        name: "Learn React",
+        detals: "Build pretty apps",
+        isCompleted: false
+    },
+    5: {
+        name: "Learn touch typing",
+        detals: "Type without typos",
+        isCompleted: false
+    },
+    6: {
+        name: "Learn OOP",
+        detals: "Build good apps",
+        isCompleted: false
+    },
+};
+
+export default data;
